77ee94aebada410c1ea3a6bec248e03c438be9d3
sjain882 <43217178+sjain882@users.noreply.github.com>
2021-11-07 15:16:19 +0000

    Initial Upload

---
+++ dllmain.cpp
@@ -0,0 +1,33 @@
+/* OMSI Presentation Tools
+*  C++20, dear ImGui, SimpleINI
+*  Created by sjain (https://github.com/sjain882)
+*  Issues at https://github.com/sjain882/OMSI-Presentation-Tools/issues
+*  Suggestions at https://github.com/sjain882/OMSI-Presentation-Tools/pulls
+*/
+
+
+
+// Imports
+
+#include <iostream>
+#include <codecvt>
+#include <vector>
+#include <math.h>
+#include "windows.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
