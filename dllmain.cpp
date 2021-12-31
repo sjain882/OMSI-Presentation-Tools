0aa78f59ba2a98b5c03fd0b290dc0850b6d90a8a
sjain882 <43217178+sjain882@users.noreply.github.com>
2021-12-31 18:01:23 +0000

    licensing

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,9 +1,22 @@
 /* OMSI Presentation Tools
  * C++17, Windows Forms
  * Created by sjain (https://github.com/sjain882)
- * Issues at https://github.com/sjain882/OMSI-Presentation-Tools/issues
- * Suggestions at https://github.com/sjain882/OMSI-Presentation-Tools/pull
-*/
+ * Issues & suggestions at:
+ * https://github.com/sjain882/OMSI-Presentation-Tools/issues
+ * 
+ * Copyright 2021 sjain882
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 
 /* Imports */
